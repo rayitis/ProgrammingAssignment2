b547c5abe72a63900b61fb4e476be42b7bd97e87
Ray Felipe <ramonfelipe4u@gmail.com>
2017-09-03 20:51:49 -0700

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,9 @@
+## These functions are for creating objects that stores a matrix and its inverse
+
+## makeCacheMatrix function accepts matrix parameter and returns an object that
+## contains a list of functions that will set and get a matrix,
+
+
 makeCacheMatrix <- function(x = matrix()) {
         theinv <- NULL
         set <- function(y) {
@@ -14,7 +20,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-
+## cacheSolve takes an object parameter returned by makeCacheMatrix and returns
+## the inverse of the matrix for the makeCacheMatrix above.  If the inverse
+## has already been calculated and the matrix has not changed, then retrieve
+## the inverse from the cache. If it hasn't been calculated, it will calculate
+## the inverse and cache it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
